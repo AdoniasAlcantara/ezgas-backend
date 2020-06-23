094fefce3391804faf4a4e7dba05ef379c000e85
AdoniasAlcantara <adoniasasneto@gmail.com>
2020-06-23 12:07:25 -0300

    Add stage profile configuration

--- src/main/kotlin/io/proj4/ezgas/controller/BrandController.kt
+++ src/main/kotlin/io/proj4/ezgas/controller/BrandController.kt
@@ -1,17 +1,15 @@
 package io.proj4.ezgas.controller
 
+import io.proj4.ezgas.model.Brand
 import io.proj4.ezgas.repository.BrandRepository
-import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/brands")
-@ResponseStatus(HttpStatus.OK)
 class BrandController(private val repository: BrandRepository) {
 
     @GetMapping
-    fun getAll() = repository.findAll()
+    fun getAll(): List<Brand> = repository.findAll()
 }
