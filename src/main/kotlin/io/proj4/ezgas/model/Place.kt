76cf4615cd316e2553c1ab13f152dae6d34913f0
AdoniasAlcantara <adoniasasneto@gmail.com>
2021-05-10 21:15:10 -0300

    fix: missing Geo Spatial index

--- src/main/kotlin/io/proj4/ezgas/model/Place.kt
+++ src/main/kotlin/io/proj4/ezgas/model/Place.kt
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.annotation.JsonSerialize
 import io.proj4.ezgas.configuration.conversion.GeoJsonConverter
 import org.springframework.data.mongodb.core.geo.GeoJsonPoint
+import org.springframework.data.mongodb.core.index.GeoSpatialIndexType
+import org.springframework.data.mongodb.core.index.GeoSpatialIndexed
 
 data class Place(
     val houseNumber: String?,
@@ -14,6 +16,7 @@ data class Place(
     val postalCode: String,
 
     @JsonSerialize(converter = GeoJsonConverter::class)
+    @GeoSpatialIndexed(type = GeoSpatialIndexType.GEO_2DSPHERE)
     val position: GeoJsonPoint,
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
