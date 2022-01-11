20dae9d9768122c01850f5911827214320df8a2f
AdoniasAlcantara <adoniasasneto@gmail.com>
2022-01-10 23:05:02 -0300

    refactor: remove unused exception

--- src/main/kotlin/io/proj4/ezgas/error/Exceptions.kt
+++ src/main/kotlin/io/proj4/ezgas/error/Exceptions.kt
@@ -5,6 +5,3 @@ import org.springframework.web.server.ResponseStatusException
 
 class ResourceNotFoundException(resourceId: Any) :
     ResponseStatusException(NOT_FOUND, "Resource not found: $resourceId")
-
-class PageNotFoundException(pageNumber: Int) :
-    ResponseStatusException(NOT_FOUND, "The requested page does not exist. Page number was $pageNumber")
