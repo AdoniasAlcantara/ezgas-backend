f4633f4525d297a2e4f1d483da4a6e89becc16c0
AdoniasAlcantara <adoniasasneto@gmail.com>
2022-01-23 19:06:38 -0300

    feat: sort fuel history from oldest to newest

--- src/main/kotlin/io/proj4/ezgas/repository/FuelHistoryRepositoryImpl.kt
+++ src/main/kotlin/io/proj4/ezgas/repository/FuelHistoryRepositoryImpl.kt
@@ -3,10 +3,10 @@ package io.proj4.ezgas.repository
 import io.proj4.ezgas.model.FuelHistory
 import io.proj4.ezgas.model.FuelType
 import org.bson.types.ObjectId
+import org.springframework.data.domain.Sort.Direction.ASC
 import org.springframework.data.mongodb.core.MongoOperations
-import org.springframework.data.mongodb.core.query
+import org.springframework.data.mongodb.core.aggregation.Aggregation.*
 import org.springframework.data.mongodb.core.query.Criteria.where
-import org.springframework.data.mongodb.core.query.Query.query
 import org.springframework.data.mongodb.core.query.isEqualTo
 import org.springframework.stereotype.Repository
 import java.time.LocalDate
@@ -20,14 +20,16 @@ class FuelHistoryRepositoryImpl(private val mongo: MongoOperations) : FuelHistor
         startDate: LocalDate,
         endDate: LocalDate
     ): List<FuelHistory> {
-        val result = mongo.query<FuelHistory>().matching(
-            query(
+        val aggregation = newAggregation(
+            FuelHistory::class.java,
+            match(
                 where(FuelHistory::stationId.name).isEqualTo(stationId)
                     .and(FuelHistory::fuelType.name).isEqualTo(fuelType)
                     .and(FuelHistory::date.name).gte(startDate).lte(endDate)
-            )
+            ),
+            sort(ASC, FuelHistory::date.name)
         )
 
-        return result.all()
+        return mongo.aggregate(aggregation, FuelHistory::class.java).mappedResults
     }
 }
