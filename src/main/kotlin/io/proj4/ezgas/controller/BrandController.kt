d50c04fdba1a1383b690b52ae6790f8531a52b7c
AdoniasAlcantara <adoniasasneto@gmail.com>
2022-01-27 19:54:35 -0300

    feat: add cache header

--- src/main/kotlin/io/proj4/ezgas/controller/BrandController.kt
+++ src/main/kotlin/io/proj4/ezgas/controller/BrandController.kt
@@ -1,15 +1,20 @@
 package io.proj4.ezgas.controller
 
-import io.proj4.ezgas.model.Brand
 import io.proj4.ezgas.repository.BrandRepository
+import org.springframework.http.CacheControl
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.TimeUnit
 
 @RestController
 @RequestMapping("/brands")
 class BrandController(private val repository: BrandRepository) {
 
     @GetMapping
-    fun getAll(): List<Brand> = repository.findAll()
+    fun getAll() =
+        ResponseEntity.ok()
+            .cacheControl(CacheControl.maxAge(7L, TimeUnit.DAYS))
+            .body(repository.findAll())
 }
